16f9da4859030f54964b0cd6a38316fe512da901
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2023-02-07 18:36:25 -0600

    authentication and deployment to the live ic

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,13 +1,37 @@
 import React, { useState } from "react";
-import {token} from "./../../../declarations/token"
+import {token, canisterId, createActor} from "./../../../declarations/token"
 import {Principal} from "@dfinity/principal";
+import { AuthClient } from "@dfinity/auth-client";
 
 function Faucet() {
   const [isDisabled, setDisabled]=useState(false);
   const [message, setMessage]= useState("Gimme gimme");
   async function handleClick(event) {
     setDisabled(true);
-    const message=await token.payOut();
+
+    // HTTP Client to authenticate the user on ic 
+    const authClient=await AuthClient.create();
+    const identity=await authClient.getIdentity(); // authenticated user
+
+     /******************************** QUESTIONS ****************************************
+     * 
+     * Why is the user a canister?
+     * Isn't it enough if the user has a principal id?
+     * Why does the createActor method use my own canisterId as a parameter?
+     * Is it because it needs to create a fully new canister 
+     * that's encrypted and unique like an https connection between the user and my dapp?
+     * 
+     ***********************************************************************************/
+
+    // create actor with the identity provided
+    const authenticatedCanisterUser = createActor(canisterId,{
+          agentOptions: {
+            identity
+          },
+    });
+
+    // The payOut method is called by the authenticated user and not this canister itself
+    const message=await authenticatedCanisterUser.payOut();
     setMessage(message);
     // setDisabled(false);
   }
--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,9 +1,26 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import { AuthClient } from '@dfinity/auth-client';
 
 const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  
+  const authClient= await AuthClient.create();
+  if(await authClient.isAuthenticated()){
+    handleAuthenticated(authClient);
+  }else{
+    await authClient.login({
+      identityProvider:"https://identity.ic0.app/#authorize",
+      onSuccess:()=>{
+        handleAuthenticated(authClient);
+      }
+    });
+  }
+
+  // This function handles app behavior after the user is logged in
+  async function handleAuthenticated(authClient){
+    ReactDOM.render(<App />, document.getElementById("root"));    
+  }
 }
 
 init();
